9ede857841cf13fb1519b5385d74dab859b5f4a6
Armando Guarino <arm.guarino@gmail.com>
2021-11-25 18:05:50 +0100

    navbar - inject dynamic values

--- src/app/components/layout/navbar/navbar.component.ts
+++ src/app/components/layout/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IProfile } from 'src/app/interfaces/interfaces';
+import { ProfileService } from 'src/app/services/profile.service';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  name: string;
+  profilePicture: string
 
-  constructor() { }
+  constructor(private profileService: ProfileService) { }
 
   ngOnInit(): void {
+    this.profileService.profile.subscribe((results) => {
+      this.name = results.name;
+      this.profilePicture = results.picture
+    });
   }
 
 }
